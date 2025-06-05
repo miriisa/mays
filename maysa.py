d8c59a02ca6fe652749aadb2aafe38a8254b6868
miriisa <maysagrazielle188@gmail.com>
2025-06-05 09:27:29 -0300

    Add files via upload

--- maysa.py
+++ maysa.py
@@ -1,4 +1,7 @@
-total = 200
-porcentagem = 15 
-total_parte = total*(porcentagem/100)
-print (total_parte)
+chat = 100
+porcentagem = 20
+chat_desconto = chat*(porcentagem/100)
+preco_final = chat - chat_desconto
+ 
+print (preco_final)
+print (chat_desconto)
